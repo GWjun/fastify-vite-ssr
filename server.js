d031cec82297ad03725e0bdfe8011141fbd8ae39
gwjun <kb865900@gmail.com>
2024-12-06 12:05:55 +0900

    feat: serve with fastify

---
+++ server.js
@@ -0,0 +1,23 @@
+import Fastify from 'fastify'
+import FastifyVite from '@fastify/vite'
+
+const server = Fastify({
+  logger: {
+    transport: {
+      target: '@fastify/one-line-logger',
+    },
+  },
+})
+
+await server.register(FastifyVite, {
+  root: import.meta.url,
+  dev: process.argv.includes('--dev'),
+  spa: true,
+})
+
+server.get('/', (req, reply) => {
+  return reply.html()
+})
+
+await server.vite.ready()
+await server.listen({ port: 3000 })
---
+++ vite.config.js
@@ -0,0 +1,7 @@
+import viteFastify from '@fastify/vite/plugin'
+import viteReact from '@vitejs/plugin-react'
+
+export default {
+  root: import.meta.dirname,
+  plugins: [viteFastify(), viteReact()],
+}
